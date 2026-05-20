8755671c9ac55466e78d4dbdd28a49fae856636c
Marion.L <mrn.larue@gmail.com>
2026-05-20 11:43:07 +0200

    Modif fichiers suite Flake8 cause fichiers vides

--- litreview/admin.py
+++ litreview/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Review, Ticket, User, UserFollows
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
