02397d607935a47b7e2aac4c425b0354058f4e45
Marion.L <mrn.larue@gmail.com>
2026-05-21 13:43:17 +0200

    Prépare la vue de connexion

--- litreview/views.py
+++ litreview/views.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 
 @login_required
 def accueil(request):
-    """Vue django"""
+    """Vue django pour obligation de connexion"""
     return render(request, "pages/accueil.html")
+
+
+def connexion(request):
+    """Affiche le formulaire de connexion."""
+    formulaire_connexion = AuthenticationForm()
+    return render(
+        request,
+        "pages/connexion.html",
+        {"formulaire_connexion": formulaire_connexion},
+    )
