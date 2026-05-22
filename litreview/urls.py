82d75de130b93aae3a8475ed0b1545ab18483291
Marion.L <mrn.larue@gmail.com>
2026-05-22 15:51:35 +0200

    Ajoute l'URL de création de ticket

--- litreview/urls.py
+++ litreview/urls.py
@@ -3,8 +3,18 @@ from django.urls import path
 from litreview import views
 
 urlpatterns = [
+    # ----------------------------
+    # Page utilisateur
+    # ----------------------------
     path("accueil/", views.accueil, name="accueil"),
+    # ----------------------------
+    # Authentification
+    # ----------------------------
     path("connexion/", views.connexion, name="connexion"),
     path("deconnexion/", views.deconnexion, name="deconnexion"),
     path("inscription/", views.inscription, name="inscription"),
+    # ----------------------------
+    # Tickets
+    # ----------------------------
+    path("creer-ticket/", views.creer_ticket, name="creer_ticket"),
 ]
