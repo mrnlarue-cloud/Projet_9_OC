5443b9022e0790a97c14881d53a632ba4f39b94c
Marion.L <mrn.larue@gmail.com>
2026-05-22 12:10:25 +0200

    Modif erreur Litreview/url

--- litreview/urls.py
+++ litreview/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("accueil/", views.accueil, name="accueil"),
     path("connexion/", views.connexion, name="connexion"),
     path("deconnexion/", views.deconnexion, name="deconnexion"),
+    path("inscription/", views.inscription, name="inscription"),
 ]
